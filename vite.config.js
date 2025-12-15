95769a6a5fdd13303dd5731668464c016e20e44c
Mohamed Firas Alfarra <fralfarra11@gmail.com>
2025-12-15 13:10:55 +0300

    Uplode all files

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import { screenGraphPlugin } from "@animaapp/vite-plugin-screen-graph";
+// import { screenGraphPlugin } from "@animaapp/vite-plugin-screen-graph";
 import react from "@vitejs/plugin-react";
 import tailwind from "tailwindcss";
 import { defineConfig } from "vite";
@@ -6,9 +6,9 @@ import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => ({
-  plugins: [react(), mode === "development" && screenGraphPlugin()],
+  plugins: [react(), mode === "development"],
   publicDir: "./static",
-  base: "./",
+  base: '/SARIAN/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
